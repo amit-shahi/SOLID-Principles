53e29ac465d62f5df3aace5d1cbb4620a49d5b5f
Amit Shahi <ami.oneworld@gmail.com>
2019-07-06 23:35:45 +0530

    Updated Demo of Single Responsibility Principle

--- Single Responsibility Principle/Program.cs
+++ Single Responsibility Principle/Program.cs
@@ -1,12 +1,74 @@
 ﻿using System;
+using static System.Console;
 
 namespace Single_Responsibility_Principle
 {
+     // just stores a couple of journal entries and ways of
+  // working with them
+  public class Journal
+  {
+    private readonly List<string> entries = new List<string>();
+
+ 
+ 
+    private static int count = 0;
+
+    public int AddAnEntry(string text)
+    {
+      entries.Add($"{++count}: {text}");
+       
+      return count; // memento pattern!
+    }
+
+    public void RemoveAnEntry(int index)
+    {
+      entries.RemoveAt(index);
+    }
+
+    public override string ToString()
+    {
+      return string.Join(Environment.NewLine, entries);
+    }
+
+    // breaks single responsibility principle
+    public void Save(string filename, bool overwrite = false)
+    {
+      File.WriteAllText(filename, ToString());
+    }
+
+   // performs loading of journal objects
+    public void Load(string filename)
+    {
+      
+    }
+
+  }
+
+  // handles the responsibility of persisting journal objects
+  public class Persistence
+  {
+    public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+    {
+      if (overwrite || !File.Exists(filename))
+        File.WriteAllText(filename, journal.ToString());
+    }
+  }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Demo of Single Responsibility Principle");
+            WriteLine("Demo of Single Responsibility Principle");
+
+            var journal = new Journal();
+            journal.AddAnEntry("I cried today.");
+            journal.AddAnEntry("I ate a bug.");
+            WriteLine(journal);
+
+            var persistence = new Persistence();
+            var filename = @"c:\\Temp\\myjournal.txt";
+            persistence.SaveToFile(journal, filename);
+            Process.Start(filename);
         }
     }
 }
