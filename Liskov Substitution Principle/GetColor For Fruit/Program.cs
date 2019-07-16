06b48dd9caa588f440b9b02c8581b6762a6d4dd3
Amit Shahi <ami.oneworld@gmail.com>
2019-07-16 23:12:11 +0530

    Added Examples for Open-Closed & Liskov Substitution principles

---
+++ Liskov Substitution Principle/GetColor For Fruit/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using static System.Console;
+
+namespace Liskov_Substitution_Principle
+{
+    //// Wrong Way
+    // public class Apple
+    // {
+    //     public virtual string GetColor()
+    //     {
+    //         return "Red";
+    //     }
+    // }
+    // public class Orange : Apple
+    // {
+    //     public override string GetColor()
+    //     {
+    //         return "Orange";
+    //     }
+    // }
+
+
+    //// Correct way using Abstract class and right substitutable base class 
+    
+    public abstract class Fruit
+    {
+        public abstract string GetColor();
+    }
+    public class Apple : Fruit
+    {
+        public override string GetColor()
+        {
+            return "Red";
+        }
+    }
+    public class Orange : Apple
+    {
+        public override string GetColor()
+        {
+            return "Orange";
+        }
+    }
+
+    public class Banana : Fruit
+    {
+        public override string GetColor()
+        {
+            return "Yellow";
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //// Wrong way
+
+            // Apple apple = new Orange();
+            // Console.WriteLine(apple.GetColor());
+
+
+            //// Correct way
+
+            Fruit fruit = new Orange();
+
+            WriteLine(fruit.GetColor()); // returns Orange Color as -> Orange
+            
+            fruit = new Apple();
+
+            WriteLine(fruit.GetColor()); // returns Apple Color as -> Red
+
+            fruit = new Banana();
+
+            WriteLine(fruit.GetColor()); // returns Banana Color as -> Yellow 
+
+        }
+    }
+}
---
+++ Open Closed Principle/InvoiceDiscount/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using static System.Console;
+
+namespace Open_Closed_Principle
+{
+        public abstract class Invoice
+        {
+            public abstract double GetInvoiceDiscount(double amount);
+        }
+    
+        public class FinalInvoice : Invoice
+        {
+            public override double GetInvoiceDiscount(double amount)
+            {
+                return amount - (amount * 0.60); // 60% invoice discount
+            }
+        }
+        public class ProposedInvoice : Invoice
+        {
+            public override double GetInvoiceDiscount(double amount)
+            {
+                return amount - (amount * 0.40); // 40% invoice discount
+            }
+        }
+        public class RecurringInvoice : Invoice
+        {
+            public override double GetInvoiceDiscount(double amount)
+            {
+                return amount - (amount * 0.30); // 30% invoice discount
+            }
+        }
+
+        public class MonthlyInvoice : Invoice
+        {
+            public override double GetInvoiceDiscount(double amount)
+            {
+                return amount - (amount * 0.20); // 20% invoice discount
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Invoice _finalInvoice = new FinalInvoice();
+                Invoice _proposedInvoice = new ProposedInvoice();
+                Invoice _recurringInvoice = new RecurringInvoice();
+                Invoice _monthlyInvoice = new MonthlyInvoice();
+
+                double _finalInvoiceAmount = _finalInvoice.GetInvoiceDiscount(10000);
+                double _proposedInvoiceAmount = _proposedInvoice.GetInvoiceDiscount(10000);
+                double _recurringInvoiceAmount = _recurringInvoice.GetInvoiceDiscount(10000);
+
+                double _monthlyInvoiceAmount = _monthlyInvoice.GetInvoiceDiscount(1000);
+
+               WriteLine();
+               WriteLine($"Total amount is $ 1000");
+               WriteLine();
+               
+               WriteLine($"Final Invoice : $ {_finalInvoiceAmount} with 60% discount.");
+               WriteLine($"Proposed Invoice Amount : $ {_proposedInvoiceAmount}  with 40% discount.");
+               WriteLine($"Recurring Invoice Amount : $ {_recurringInvoiceAmount}  with 30% discount.");
+               WriteLine($"Monthly Invoice Amount : $ {_monthlyInvoiceAmount}  with 20% discount.");
+                
+                
+            }
+        }
+}
